0449e3d43f6f63a43584e24c84d8cc37d0e0239b
Anirudh <34083360+1bharadvaja@users.noreply.github.com>
2025-02-09 22:11:17 -0800

    hermite coeff

--- compute.cu
+++ compute.cu
@@ -28,3 +28,54 @@ __device__ double boys_function(int m, double T) {
     }
 }
 
+__device__ void computeHermiteCoeffs(int la, int lb, double P, double base,
+                                     double A, double B, double p, double *E_out) {
+    int size_i = la + 1;
+    int size_j = lb + 1;
+    int total_size = size_i * size_j;
+    // Temporary array F[i][j] stored in 1D (assume total_size <= 100)
+    double F[100];
+    for (int i = 0; i < total_size; i++) {
+        F[i] = 0.0;
+    }
+    //base case
+    F[0] = base;
+    
+    for (int i = 1; i < size_i; i++) {
+        double term1 = (P - A) * F[(i-1)*size_j + 0];
+        double term2 = 0.0;
+        if (i - 1 >= 1) {
+            term2 = ((double)(i - 1))/(2.0 * p) * F[(i-2)*size_j + 0];
+        }
+        F[i*size_j + 0] = term1 + term2;
+    }
+    
+    for (int j = 1; j < size_j; j++) {
+        double term1 = (P - B) * F[0*size_j + (j-1)];
+        double term2 = 0.0;
+        if (j - 1 >= 1) {
+            term2 = ((double)(j - 1))/(2.0 * p) * F[0*size_j + (j-2)];
+        }
+        F[0*size_j + j] = term1 + term2;
+    }
+    // recurrence
+    for (int i = 1; i < size_i; i++) {
+        for (int j = 1; j < size_j; j++) {
+            double term1 = (P - A) * F[(i-1)*size_j + j];
+            double term2 = (P - B) * F[i*size_j + (j-1)];
+            double term3 = ((double)(i+j-1))/(2.0 * p) * F[(i-1)*size_j + (j-1)];
+            F[i*size_j + j] = term1 + term2 + term3;
+        }
+    }
+    // sum reccurence
+    int L = la + lb;
+    for (int t = 0; t <= L; t++) {
+        double sum = 0.0;
+        for (int i = 0; i <= la; i++) {
+            int j = t - i;
+            if (j < 0 || j > lb) continue;
+            sum += F[i*size_j + j];
+        }
+        E_out[t] = sum;
+    }
+}
